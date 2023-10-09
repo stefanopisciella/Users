a091059ebbebc40ffc4cfd3a45579b467e351703
stefanopisciella <stefano.pisciella1@student.univaq.it>
2023-10-09 15:53:24 +0200

    m

--- application/views/users.php
+++ application/views/users.php
@@ -9,32 +9,35 @@
       <div class="modal-body">
         <!-- modal content -->
         <div class="row">
-            <form class="form-horizontal">
-                <div class="form-group">
-                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+            <div class="col-xs-11"> <!-- we give 11 columns to the form so that it its inputs don't overflow -->
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                    <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                        </div>
                     </div>
-                </div>
-            
-              
-               
-            </form>
+                
+                
+                
+                </form>
+            </div>
         </div>
         <div class="row">
-                    <select class="form-control">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
+            <div class="col-xs-2"> <!-- we give 2 columns to the select for resizing it (because by default this select is too large) -->
+                <select class="form-control">
+                    <option>-</option> <!-- default option -->
+                    <?php for($i=2005;$i>=1930;$i--): ?> 
+                    <?php   echo '<option>' . $i . '</option>'; ?>
+                    <?php endfor; ?>
+                </select>
+            </div>
         </div>
         <div class="row">
             <div class="form-group">
