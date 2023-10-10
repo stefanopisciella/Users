20b5449a516bfb9d8de982ab7f3a08d0893edaa1
stefanopisciella <stefano.pisciella1@student.univaq.it>
2023-10-10 21:40:07 +0200

    removed pagination and user order

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -9,66 +9,23 @@ use function PHPSTORM_META\sql_injection_subst;
 class User extends AbastractController{
     public static function remove() {
         $user_id = $GLOBALS['f3']->get('PARAMS.id');
+        
         // i should check wether the $article_id contains an integer
         
         ModelUser::remove($user_id); 
 
-        $GLOBALS['f3']->reroute('/user');
     }
 
     public static function index() {
-        if($GLOBALS['f3']->exists('GET.page')) {
-            $current_page = $GLOBALS['f3']->get('GET.page');
-        } else {
-            $current_page = 1; // if the url doesn't contain the "page" parameter, by default, to the user will be showed the first page
-        }
+        $users = ModelUser::index(); 
+
+        $user_table = User::makeUserTable($users);
         
-        // $order is relative to the currently requested page, it's not relative to the previously requested page
-        if($GLOBALS['f3']->exists('GET.order')) {
-            $order = $GLOBALS['f3']->get('GET.order'); // $order indicates the order of the articles of the current page 
-        } else {
-            // it considers the default order (it orders by the creation_datetime column of the DB table)
-            $order = 1; 
-        }
-
-        // $dir is relative to the currently requested page, it's not relative to the previously requested page
-        if($GLOBALS['f3']->exists('GET.dir')) {
-            $dir = $GLOBALS['f3']->get('GET.dir');
-        } else {
-            // it considers the default direction DESC
-            $dir = 1;  
-        }
-
-        if($GLOBALS['f3']->exists('GET.search')) {
-            $keywords = $GLOBALS['f3']->get('GET.search');
-
-            $GLOBALS['f3']->set('search_param', '&search=' . $keywords); // it adds the search param into the href attribute of the order button
-            $GLOBALS['f3']->set('results_for_heading', '<h3><small>Results for: </small>' . $keywords . '</h3>'); 
-
-            $users = ModelUser::search($keywords, $current_page, $GLOBALS['max_num_of_articles_per_page'], $order, $dir);
-            $total_num_of_users = ModelUser::getNumOfMatchingUsers($keywords);
-
-            $GLOBALS['f3']->set('results_num_label', '<label><small>' . $total_num_of_users .  ' results</small></label>'); 
-        } else {
-            $keywords = "";
-
-            $GLOBALS['f3']->set('search_param', ''); // it doesn't add the search param into the href attribute of the order button
-            $GLOBALS['f3']->set('results_for_heading', ''); 
-
-            $users = ModelUser::index($current_page, $GLOBALS['max_num_of_users_per_page'], $order, $dir); 
-            $total_num_of_users = ModelUser::getNumOfUsers();
-
-            $GLOBALS['f3']->set('results_num_label', ''); // hide the results_num label
-        }
-
-        parent::definePagination($GLOBALS['max_num_of_users_per_page'], $total_num_of_users , $GLOBALS['url_prefix'] . "user", $current_page, $order, $dir, $keywords);
-
-        $user_table = User::makeUserTable($users, $order, $dir);
         $GLOBALS['f3']->set('page_content', $user_table); // inject user_table into layout
         echo $GLOBALS['view']->render('application/layouts/layout.html');
     }
 
-    public static function makeUserTable($users, $order, $dir) {
+    public static function makeUserTable($users) {
         $num_users = sizeof($users);
         if($num_users > 0) {
             $table_rows= Array();
@@ -85,9 +42,6 @@ class User extends AbastractController{
                 array_push($table_rows, $table_row);
             }
             $GLOBALS['f3']->set('table_rows', $table_rows); // inject rows within the user_table
-            
-            User::setTheDirectionOfUsersOrder($order, $dir);
-            
             $user_table = $GLOBALS['view']->render('application/views/user_table.html'); 
 
         } else{
@@ -97,7 +51,10 @@ class User extends AbastractController{
         return $user_table;
     }
 
-    public static function sendUserTableToClient($user_table) {
+    public static function sendUserTableToClient() {
+        $users = ModelUser::index(); 
+        $user_table = User::makeUserTable($users);
+
         $data = array();
         $data['html'] = $user_table;
         
@@ -112,53 +69,9 @@ class User extends AbastractController{
         echo json_encode($response);
     }
 
-
     public static function renderLayout() {
         echo parent::render('application/layouts/layout.html'); // inject user_table within the user.html page */
     }
-
-
-    public static function setTheDirectionOfUsersOrder($order, $dir) {
-        // order = 1 means that it orders by the the creation_datetime column of the DB table
-        // order = 2 means that it orders by the the last_update_datetime column of the DB table
-        // dir = 1 is DESC
-        // dir = 2 is ASC
-        
-        if($order == 1) {
-            if($dir == 1) {
-                $GLOBALS['f3']->set('order1_dir', 2); // switch the DIR
-
-                $GLOBALS['f3']->set('direction_arrow_order1', '&darr;'); // ⬇️
-            } else if ($dir == 2) {
-                $GLOBALS['f3']->set('order1_dir', 1); // switch the DIR
-
-                $GLOBALS['f3']->set('direction_arrow_order1', '&uarr;'); // ⬆️
-            }
-            
-            $GLOBALS['f3']->set('order2_dir', 1);
-
-            $GLOBALS['f3']->set('direction_arrow_order2', '');
-        } else if ($order == 2) {
-            if($dir == 1) {
-                $GLOBALS['f3']->set('order2_dir', 2); // switch the DIR
-
-                $GLOBALS['f3']->set('direction_arrow_order2', '&darr;'); // ⬇️
-            } else if ($dir == 2) {
-                $GLOBALS['f3']->set('order2_dir', 1); // switch the DIR
-
-                $GLOBALS['f3']->set('direction_arrow_order2', '&uarr;'); // ⬆️
-            }
-
-            $GLOBALS['f3']->set('order1_dir', 1);
-
-            $GLOBALS['f3']->set('direction_arrow_order1', '');
-        } 
-    }
-
-    
-
-
-
 
 
 }
