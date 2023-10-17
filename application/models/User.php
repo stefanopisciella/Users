18336c3d6e52361a2c31ac35abf296392b35c26b
stefanopisciella <stefano.pisciella1@student.univaq.it>
2023-10-17 12:41:29 +0200

    m

--- application/models/User.php
+++ application/models/User.php
@@ -30,17 +30,14 @@ class User {
     public static function save($user) {
         if($user['user_id'] == "not_set") {
             // CREATION of the user
-            
-            //
             $query = "INSERT INTO user(name, email, birth_year, is_male, privacy_agreed) VALUES ('{$user["name"]}','{$user["email"]}', {$user["birth_year"]}, {$user["is_male"]}, {$user["privacy_agreed"]})";
-
             $GLOBALS['f3']->get('DB')->exec($query);
         } else {
             // UPDATE of the user
             $GLOBALS['f3']->get('DB')->exec(
                 "UPDATE user
-                 SET name = '{$user["name"]}', email = '{$user["email"]}', birth_year = '{$user["birth_year"]}', is_male = '{$user["is_male"]}', privacy_agreed = '{$user["privacy_agreed"]}'
-                 WHERE ID = '{$user["user_id"]}';"
+                 SET name = '{$user["name"]}', email = '{$user["email"]}', birth_year = {$user["birth_year"]}, is_male = {$user["is_male"]}, privacy_agreed = {$user["privacy_agreed"]}
+                 WHERE ID = {$user["user_id"]};"
             );
         }
     }
